355bb0cb4b96ef670c16df079c861108fff298a1
Roberts Sukonovs <roberts.sukonovs@tet.lv>
2020-09-28 18:22:55 +0300

    Add put/patch, create middleware.

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -16,17 +16,60 @@ function routes(Book) {
         return res.json(books);
       });
     })
-    .post(((req, res) => {
+    .post((req, res) => {
       const book = new Book(req.body);
       book.save();
 
       return res.status(201)
         .json(book);
-    }));
+    });
+
+  bookRouter.use('/books/:bookId', (req, res, next) => {
+    Book.findById(req.params.bookId, (err, book) => {
+      if (err) {
+        return res.send(404);
+      }
+      req.book = book;
+
+      return next();
+    });
+  });
 
   bookRouter.route('/books/:bookId')
     .get((req, res) => {
-      Book.findById(req.params.bookId, (err, book) => {
+      const { book } = req;
+      res.json(book);
+    })
+    .put((req, res) => {
+      const { book } = req;
+      book.set('title', req.body.title);
+      book.set('author', req.body.author);
+      book.set('genre', req.body.genre);
+      book.set('read', req.body.read);
+
+      book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
+    })
+    .patch((req, res) => {
+      const { book } = req;
+
+      // eslint-disable-next-line no-underscore-dangle
+      if (req.body._id) {
+        // eslint-disable-next-line no-underscore-dangle
+        delete req.body._id;
+      }
+
+      Object.entries(req.body).forEach((item) => {
+        const key = item[0];
+        const value = item[1];
+        book[key] = value;
+      });
+
+      book.save((err) => {
         if (err) {
           return res.send(err);
         }
