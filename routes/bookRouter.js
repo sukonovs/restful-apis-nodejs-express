435c475753db9a7ada7ae170dddb21b0d8f38cc4
Roberts Sukonovs <roberts.sukonovs@tet.lv>
2020-09-29 17:33:22 +0300

    Move functions to controller.

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,28 +1,13 @@
 const express = require('express');
+const booksController = require('../controllers/booksController');
 
 function routes(Book) {
   const bookRouter = express.Router();
+  const controller = booksController(Book);
 
   bookRouter.route('/books')
-    .get((req, res) => {
-      const query = {};
-      if (req.query.genre) {
-        query.genre = req.query.genre;
-      }
-      Book.find(query, (err, books) => {
-        if (err) {
-          return res.send(err);
-        }
-        return res.json(books);
-      });
-    })
-    .post((req, res) => {
-      const book = new Book(req.body);
-      book.save();
-
-      return res.status(201)
-        .json(book);
-    });
+    .get(controller.get)
+    .post(controller.post);
 
   bookRouter.use('/books/:bookId', (req, res, next) => {
     Book.findById(req.params.bookId, (err, book) => {
